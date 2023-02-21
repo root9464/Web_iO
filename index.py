ce4cea33150480d97e9c968041fc6e96417d32d8
AstelliaYourTiger <104570588+AstelliaYourTiger@users.noreply.github.com>
2023-02-21 21:02:46 +0500

    .push 21.02.2023

--- index.py
+++ index.py
@@ -11,6 +11,37 @@ login_manager.init_app(app)
 app.config.update(
     SECRET_KEY = 'iO'
 )
+errors = [
+        { #0
+        'error': '404',
+        'desc':'Страница не найдена',
+        },
+
+        {#1
+        'error': '401',
+        'desc':'Пользователя не существует/ не зарегестрирован ',
+        },
+
+        {#2
+        'error': '418',
+        'desc':' Я - чайник :)',
+        },
+
+        {#3
+        'error': '500',
+        'desc':'Ошибка сервера, попробуйте позже',
+        },
+        
+        {#4
+        'error': '503',
+        'desc':'Сервис недоступен',
+        },
+
+        {#5
+        'error': '521',
+        'desc':'Веб сервер не работает (поддержка - 8.950.183.11.00)',
+        },
+    ]
 
 class User(UserMixin):
     def __init__(self, id):
@@ -19,6 +50,53 @@ class User(UserMixin):
 def load_user(user_id):
     return User(user_id)
 
+#registers
+@app.route('/form/' )
+def form_get():
+    return render_template('form.html')
+
+
+@app.route('/login/', methods=['POST'])
+def login_post_form():
+    print(request.form)
+    i = db.users.get("login", request.form["login"])
+    if not i:
+         return redirect("http://127.0.0.1:5000/error")
+    if request.form['login'] != i.login:
+        return render_template('error404.html') #errors
+    if request.form['password'] != i.password:
+        return render_template('error404.html')
+    _user = User(request.form['login'])
+    login_user(_user)
+
+
+    return redirect("http://127.0.0.1:5000/app")
+
+
+@app.route('/register/', methods=['POST'])
+def register(): 
+    print(request.form)
+    if not request.form["password"] or not request.form["login"]:
+        return redirect("http://127.0.0.1:5000/400") # error 4...
+    try:
+        db.users.put(request.form)
+    except:
+        return redirect("http://127.0.0.1:5000/400") # error 400
+    _user = User(request.form["email"])
+    login_user(_user)
+    return redirect('http://127.0.0.1:5000/app')
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("http://127.0.0.1:5000/form")
+
+
+
+
+#pages
 
 @app.route('/app/')
 @login_required
@@ -30,26 +108,19 @@ def index():
     ]
     return render_template('app.html', statick=statick)
 
-
 @app.route('/progress/')
 @login_required
 def progress():
      return render_template('progress.html')
-
-
-
-
-
-
 
 @app.route('/price/')
 @login_required
 def price_list():
     price = [
         {
-        'price': '1500',
-        'name_price_tag': 'Premium',
-        'description': 'lorem ipsum dolor sit am',
+            'price': '1500',
+            'name_price_tag': 'Premium',
+            'description': 'lorem ipsum dolor sit am',
         },
         {
             'price': '1000',
@@ -75,75 +146,39 @@ def products():
 def contacts():
     return render_template('contact.html')
 
-@app.route('/form/' )
-def form_get():
-    img = [
-        {
-        'image_one': '',
-        'image_three': '',
-        'image_four': '',
-        'image_five': '',
-        'image_six': '',
-        'image_seven': '',
-        }
-    ]  
-    return render_template('form.html', img = img)
-
-@app.route('/login/', methods=['POST'])
-def login_post_form():
-    print(request.form)
-    i = db.users.get("login", request.form["login"])
-    if not i:
-         return redirect("http://127.0.0.1:5000/error")
-    
-    if request.form['login'] != i.login:
-        return render_template('error404.html') #errors
-    if request.form['password'] != i.password:
-        return render_template('error404.html')
-    _user = User(request.form['login'])
-    login_user(_user)
-
-
-    return redirect("http://127.0.0.1:5000/app")
-
-@app.route('/register/', methods=['POST'])
-def register(): 
-    print(request.form)
-    if not request.form["password"] or not request.form["login"]:
-        return redirect("http://127.0.0.1:5000/error") # error 4...
-    try:
-        db.users.put(request.form)
-    except:
-        return redirect("http://127.0.0.1:5000/error") # error 400
-    _user = User(request.form["email"])
-    login_user(_user)
-
-    return redirect('http://127.0.0.1:5000/app')
-
-# @app.route('/calc/<int:a>/<int:b>/')
-# def calc(a, b):
-#     return f'{a} + {b} = {a + b}<br />{a} - {b} = {a - b}<br />' \
-#            f'{a} * {b} = {a * b}<br />{a} / {b} = {a / b}<br />' \
-#            f'{a} // {b} = {a // b}<br />{a} % {b} = {a % b}'
-
-# @app.route('/<a>')
-# def f(a):
-#     a=a.replace(':','/')
-#     a=a.replace('^','**')
-#     return a + '=' + str(eval(a))
-@app.route("/logout")
+@app.route('/authors/')
 @login_required
-def logout():
-    logout_user()
-    return redirect("http://127.0.0.1:5000/form")
+def authors():
+    return render_template('authors.html')
 
-@app.route('/error/')
-def error():
-    return render_template("errors/error404.html")
+
+
+@app.route('/401/')
+def error401():
+    return render_template("errors/error401.html", error = errors)
+@app.route('/404/')
+def error404():
+    return render_template("errors/error404.html", error = errors)
+@app.route('/418/')
+def error418():
+    return render_template("errors/error418.html", error = errors)
+@app.route('/500/')
+def error500():
+    return render_template("errors/error500.html", error = errors)
+@app.route('/503/')
+def error503():
+    return render_template("errors/error503.html", error = errors)
+@app.route('/521/')
+def error521():
+    return render_template("errors/error521.html", error = errors)
+
+
+
+
 
 @app.route('/api/orders')
 def api():
-    return jsonify(io)
+    return jsonify()
 
 if __name__ == "__main__":
     app.run()
